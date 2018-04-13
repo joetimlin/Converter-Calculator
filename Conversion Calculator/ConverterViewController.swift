597c060e9ac78e01e15630849aee9cd6287eb38e
Joe Timlin <jhthx9@mail.missouri.edu>
2018-04-13 17:00:50 -0500

    Finished UI and IBOutlets

--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class ConverterViewController: UIViewController {
 
+    @IBOutlet weak var inputDisplay: UITextField!
+    @IBOutlet weak var outputDisplay: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func converterButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Choice", message: "Choose!", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            //handle choice
+            self.inputDisplay.text = ""
+        }))
+        alert.addAction(UIAlertAction(title: "celcius to fahrenheit", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            //handle choice
+            self.inputDisplay.text = ""
+        }))
+        alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            //handle choice
+            self.inputDisplay.text = ""
+        }))
+        alert.addAction(UIAlertAction(title: "kilometers to miles", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            //handle choice
+            self.inputDisplay.text = ""
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
 
